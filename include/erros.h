717b1a6e8ec4c8e999dc1bceda1fde1f5e175947
Erick Winck <erickwinck5@gmail.com>
2025-08-03 17:39:47 -0300

    feat: adiciona códigos de erro para cabeçalho e nós

--- include/erros.h
+++ include/erros.h
@@ -13,10 +13,14 @@
 typedef enum {
         SUCESSO = 0,
 
-        ERRO_ARQUIVO_NULO = -1, /**< Handle para arquivo é nulo. */
-        ERRO_ARQUIVO_SEEK = -2, /**< Erro na função fseek. */
-        ERRO_ARQUIVO_READ = -3, /**< Erro na função fread. */
-        ERRO_ARQUIVO_WRITE = -4 /**< Erro na função fwrite. */
+        ERRO_ARQUIVO_NULO = -1,  /**< Handle para arquivo é nulo. */
+        ERRO_ARQUIVO_SEEK = -2,  /**< Erro na função fseek. */
+        ERRO_ARQUIVO_READ = -3,  /**< Erro na função fread. */
+        ERRO_ARQUIVO_WRITE = -4, /**< Erro na função fwrite. */
+
+        ERRO_CABECALHO_NULO = -10, /**< Cabeçalho não encontrado no arquivo binário. */
+
+        ERRO_NO_NULO = -20 /**< Nó da árvore é nulo. */
 } codigo_erro;
 
 #endif  // ERROS_H
